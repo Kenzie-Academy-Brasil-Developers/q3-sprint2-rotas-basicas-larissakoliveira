5e82f4a9a5f798316e3cc7273bb7d677f6b9532f
larissakoliveira <oliveir5uwm@gmail.com>
2021-12-14 17:48:43 -0300

    fix:adding text to the env.example file, changing req file name, and returning a dict in the func

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def dict():
-    return {"data": "Hello Flask"}
+    return {"data": "Hello Flask!"}
 
 @app.route('/current_datetime', methods=['GET'])
 def current_datetime_dict():
+    message = ''
     current_datetime = datetime.now()
     if current_datetime.strftime("%p") == 'am':
-        return f' {current_datetime} + <h1>Bom dia!</h1>'
+        message = 'Bom dia!'
     elif current_datetime.strftime("%H") >= '12' and current_datetime.strftime("%H") < '18':
         print(current_datetime.strftime("%H"))
-        return f'{current_datetime}  <h1>Boa tarde!</h1>'
+        message = 'Boa tarde!'
     else:
-        return f'{current_datetime}  <h1>Boa noite!</h1>'
-
+       message = 'Boa noite!'
+    return {"current_datetime": current_datetime.strftime("%d/%m/%Y %H:%M:%S %p"), "message": message}
